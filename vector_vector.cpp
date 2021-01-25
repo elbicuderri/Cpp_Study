310db18c81a7a6f33cbcbc0ab185e146cb379084
elbicuderri <muger1031@gmail.com>
2021-01-25 21:24:06 +0900

    2d vector

---
+++ vector_vector.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<std::vector<int>> aa(4);
+
+    std::vector<int> a = {1, 2, 3, 4};
+
+    for (int i = 0; i < 4; ++i) {
+        aa.emplace_back(a);
+    }
+
+    for (int r = 0; r < 4; ++r) {
+        for (int w = 0; w < 4; ++w) {
+            std::cout << "[" << r << "]" << "[" << w << "]: ";
+            printf("%p\n", &aa[r][w]);
+            // std::cout << &(aa[r][w]) << std::endl;
+        }
+    }
+
+    return 0;
+}
